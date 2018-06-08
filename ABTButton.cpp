b5503dfde3615b1b99cb501dd86b5b58be21184d
ANTBOT001 <584860790@qq.com>
2018-06-08 17:41:38 +0800

    Update ABTButton.cpp

--- ABTButton.cpp
+++ ABTButton.cpp
@@ -1,36 +1,29 @@
 /**
- * \class ABTUltrasonicSensor
+ * \class ABTButton
  * \brief   Driver for ultrasonic sensor device.
- * @file    ABTUltrasonicSensor.cpp
+ * @file    ABTButton.cpp
  * @author  ANTBOT
  * @version V1.0.1
  * @date    2017/9/10
  * @brief   Driver for Me ultrasonic sensor device. 
  */
-#include "ABTUltrasonicSensor.h"
+#include "ABTButton.h"
 
 
-ABTUltrasonicSensor::ABTUltrasonicSensor(unsigned char port)
+ABTButton::ABTButton(unsigned char port):ABTPort(port)
 {
   _SignalPin = port;
+  //s1=pin1();
+  //s2=pin2();
 }
-double ABTUltrasonicSensor::distanceCm(int MAXcm)
+unsigned char ABTButton::GetButton1()
 {
-  long distance = measure(MAXcm * 55 + 200);
-  return( (double)distance / 58.0);
+  pinMode(s1, INPUT);
+  return digitalRead(s1);
 }
-
-long ABTUltrasonicSensor::measure(unsigned long timeout)
+unsigned char ABTButton::GetButton2()
 {
-  long duration;
-  ABTPort::dWrite1(LOW);
-  delayMicroseconds(2);
-  ABTPort::dWrite1(HIGH);
-  delayMicroseconds(10);
-  ABTPort::dWrite1(LOW);
   pinMode(s2, INPUT);
-  duration = pulseIn(s2, HIGH, timeout);
-  delayMicroseconds(200);
-  return(duration);
+  return digitalRead(s2);
 }
 
