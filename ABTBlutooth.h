081da73045c5b893110af95ad865cd7ae7e79044
ANTBOT001 <584860790@qq.com>
2018-06-08 17:40:51 +0800

    Update ABTBlutooth.h

--- ABTBlutooth.h
+++ ABTBlutooth.h
@@ -1,27 +1,57 @@
-/** 
- * \brief   Driver for ultrasonic sensor device.
- * @file    ABTUltrasonicSensor.h
+/**
+ * \class ABTblutooth
+ * \brief   Driver for bluetooth moudle.
+ * @file    ABTblutooth.h
  * @author  ANTBOT
- * @version V1.0.0
- * @date    2017/09/10
- * @brief   Header for for ABTUltrasonicSensor.cpp module 
+ * @version V1.0.1
+ * @date    2018/2/24
+ * @brief   Driver for bluetooth moudle. 
  */
-#ifndef ABTUltrasonicSensor_H
-#define ABTUltrasonicSensor_H
+#ifndef ABTBLUTOOTH_H
+#define ABTBLUTOOTH_H
 
 
 #include <Arduino.h>
 #include "ABTPort.h"
 
+struct infoFrmBle{
+	char cRW;
+	int sID;
+	int sVal;
+	};
 
 
-class ABTUltrasonicSensor : public ABTPort
+class ABTBlutooth : public ABTPort
 {
 public:
 
-  ABTUltrasonicSensor(uint8_t port);
-  double distanceCm(uint16_t = 400);
-  long measure(unsigned long = 30000);
+  ABTBlutooth(uint8_t port);  
+	void ABTINIT();
+		int ABTGetBleCmd();//接收蓝牙指令
+		void ABTHandleBleCmd();//处理蓝牙指令
+	  void ABTSendCMD(char *p);//向蓝牙发送指令
+	  void ABTSimple();//透传测试
+	  
+	  void ABTPrint();
+	  
+	  void ABTSensorFunc(char cRW,int sID,int sVal);//传感器模块处理函数
+public:
+		char readcmd[64];//接收到的蓝牙数据缓存
+		char sendcmd[32];//发送缓存
+		int  speedL;
+		int  speedR;
+		infoFrmBle curInfo;		
+		infoFrmBle senInfo[8];//传感器指令缓存
+		unsigned char senCnt;	
+	private:
+		void ABTExpCmd(char * p);//解析单条帧命令
+		void ABTSprint(char *p);//调试接口
+		
+	private:	
+		
+		char HEADFRAME[4];//帧头
+		int frameCnt;//接收帧计数
+		int rbytes;	
 private:
   unsigned char  _SignalPin;
 };
